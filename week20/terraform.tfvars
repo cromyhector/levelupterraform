5da3f209771c6639ee2b6788a6d6d2dd9d0d9ae7
Claude R. Hector <cromyhector@gmail.com>
2022-05-30 20:58:20 +0000

    week 20 terraform project

---
+++ week20/terraform.tfvars
@@ -0,0 +1,2 @@
+aws_access_key = "AKIARHXMK2GNPHP463MC"
+aws_secret_key = "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"
---
+++ week20/vpc.tf
@@ -0,0 +1,7 @@
+resource "aws_vpc" "ecs_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "vpc_ecs_week20project"
+  }
+}
